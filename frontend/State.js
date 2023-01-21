02db854e7c9a21594d123f7905750319805481b5
Michael <osujim2012@gmail.com>
2023-01-21 15:59:27 -0500

    Create State.js

---
+++ frontend/State.js
@@ -0,0 +1,29 @@
+class State {
+    // key - value pairs
+    // key : symbol
+    // value : # of orders currently in this state
+
+    /*
+    {
+    TSX: {GOOG: 2},
+    AEQ: {GOOG: 1},.
+    }
+    */
+
+    constructor() {
+        this.exchangeMap = {};
+    }
+    /*
+    increment -> bool
+    symbol -> string
+    */
+    operate(exchange, symbol, increment) {
+        if(!this.exchangeMap.has(exchange)) {
+            this.exchangeMap[exchange] = new Map();
+        }
+        if(!this.exchangeMap[exchange].has(symbol)) {
+            this.exchangeMap[exchange][symbol] = new Map();
+        }
+        this.exchangeMap[exchange][symbol] += increment ? 1 : -1;
+    }
+}
