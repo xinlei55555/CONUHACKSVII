0911d405cb5e17531bc6e1c63f3d6e502f07725a
Michael <osujim2012@gmail.com>
2023-01-22 04:12:55 -0500

    whatever

---
+++ michael-cpp/processdata.hpp
@@ -0,0 +1,14 @@
+//
+//  processdata.hpp
+//  
+//
+//  Created by Michael Osuji on 2023-01-22.
+//
+
+#ifndef processdata_hpp
+#define processdata_hpp
+
+#include <stdio.h>
+
+
+#endif /* processdata_hpp */
---
+++ michael-cpp/processthedata/processthedata/main.cpp
@@ -0,0 +1,27 @@
+//
+//  processdata.cpp
+//
+//
+//  Created by Michael Osuji on 2023-01-22.
+//
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+struct transaction {
+    int difference;
+    string orderID;
+    string type;
+    string symbol;
+    string exchange;
+}
+int main() {
+    cout << "hello";
+    ifstream myfile ("AlphaData.json");
+    string line;
+    int startEpoch = 1673015280;
+    getline(myfile, line);
+    while(getline(myfile, line)) {
+        
+    }
+}
