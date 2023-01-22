b0f74a9af1e85de9a60c1112316815729b578efa
Michael <osujim2012@gmail.com>
2023-01-22 07:52:46 -0500

    move modal logic to manager

--- frontend/main.js
+++ frontend/main.js
@@ -7,29 +7,19 @@ const inputHandler = function (e) {
 
 searchSymbols.addEventListener("input", inputHandler);
 //Popup Modal Box
-var modal = document.getElementById("myModal");
+
 
 // Modal Demo Code, for testing only
 const btn = document.getElementById("b8c529be-9283-11ed-ad3c-047c16291a22");
 function openPopup(e) {
   // get ID
   let id = e.target.id;
-  modal.style.display = "block";
-  console.log(modal.style.display);
+  
 }
 
-btn.addEventListener("click", openPopup);
-var closeBtn = document.getElementById("close");
-closeBtn.onclick = function () {
-  modal.style.display = "none";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-fetch("good_data1.json")
+
+
+fetch("final_data.json")
   .then((response) => response.json())
   .then((data) => {
     const manager = new Manager(1000);
