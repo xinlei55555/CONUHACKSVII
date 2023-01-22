db209ecb2bf42c8f0bfc5f76f307b5238d04cd47
Michael <osujim2012@gmail.com>
2023-01-21 19:07:27 -0500

    Create Displayer.js

---
+++ frontend/Displayer.js
@@ -0,0 +1,12 @@
+class Displayer {
+    constructor(manager) {
+        this.symbolsToDisplay = new Set();
+        this.exchangesToDisplay = new Set();
+        this.manager = manager;
+    }
+
+    updateDisplay() {
+        const states = this.manager.States;
+
+    }
+}
